484881453d2ad5637bec3fef619771e5d42b5eca
Jonathan Rim <jonrim@umich.edu>
2016-05-26 11:12:30 -0400

    improper access

--- server/api/stories/story.router.js
+++ server/api/stories/story.router.js
@@ -17,25 +17,29 @@ router.param('id', function (req, res, next, id) {
 });
 
 router.get('/', function (req, res, next) {
-  Story.findAll({
-    include: [{model: User, as: 'author'}],
-    attributes: {exclude: ['paragraphs']}
-  })
-  .then(function (stories) {
-    res.json(stories);
-  })
-  .catch(next);
+  if (req.user) {
+    Story.findAll({
+      include: [{model: User, as: 'author'}],
+      attributes: {exclude: ['paragraphs']}
+    })
+    .then(function (stories) {
+      res.json(stories);
+    })
+    .catch(next);
+  }
 });
 
 router.post('/', function (req, res, next) {
-  Story.create(req.body)
-  .then(function (story) {
-    return story.reload({include: [{model: User, as: 'author'}]});
-  })
-  .then(function (includingAuthor) {
-    res.status(201).json(includingAuthor);
-  })
-  .catch(next);
+  if (req.user) {
+    Story.create(req.body)
+    .then(function (story) {
+      return story.reload({include: [{model: User, as: 'author'}]});
+    })
+    .then(function (includingAuthor) {
+      res.status(201).json(includingAuthor);
+    })
+    .catch(next);
+  }
 });
 
 router.get('/:id', function (req, res, next) {
@@ -47,19 +51,23 @@ router.get('/:id', function (req, res, next) {
 });
 
 router.put('/:id', function (req, res, next) {
-  req.story.update(req.body)
-  .then(function (story) {
-    res.json(story);
-  })
-  .catch(next);
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    req.story.update(req.body)
+    .then(function (story) {
+      res.json(story);
+    })
+    .catch(next);
+  }
 });
 
 router.delete('/:id', function (req, res, next) {
-  req.story.destroy()
-  .then(function () {
-    res.status(204).end();
-  })
-  .catch(next);
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    req.story.destroy()
+    .then(function () {
+      res.status(204).end();
+    })
+    .catch(next);
+  }
 });
 
 module.exports = router;
--- server/api/users/user.router.js
+++ server/api/users/user.router.js
@@ -17,11 +17,13 @@ router.param('id', function (req, res, next, id) {
 });
 
 router.get('/', function (req, res, next) {
-  User.findAll({})
-  .then(function (users) {
-    res.json(users);
-  })
-  .catch(next);
+  if (req.user) {
+    User.findAll({})
+    .then(function (users) {
+      res.json(users);
+    })
+    .catch(next);
+  }
 });
 
 router.post('/', function (req, res, next) {
@@ -33,27 +35,37 @@ router.post('/', function (req, res, next) {
 });
 
 router.get('/:id', function (req, res, next) {
-  req.requestedUser.reload({include: [Story]})
-  .then(function (requestedUser) {
-    res.json(requestedUser);
-  })
-  .catch(next);
+  if (req.user.id === req.params.id) {
+    req.requestedUser.reload({include: [Story]})
+    .then(function (requestedUser) {
+      res.json(requestedUser);
+    })
+    .catch(next);
+  }
 });
 
 router.put('/:id', function (req, res, next) {
-  req.requestedUser.update(req.body)
-  .then(function (user) {
-    res.json(user);
-  })
-  .catch(next);
+  if (req.user.id === req.params.id) {
+    req.requestedUser.update(req.body)
+    .then(function (user) {
+      res.json(user);
+    })
+    .catch(next);
+  }
 });
 
 router.delete('/:id', function (req, res, next) {
-  req.requestedUser.destroy()
-  .then(function () {
-    res.status(204).end();
-  })
-  .catch(next);
+  console.log(req.user)
+  if (req.user.isAdmin) {
+    req.requestedUser.destroy()
+    .then(function () {
+      res.status(204).end();
+    })
+    .catch(next);
+  }
+  else {
+    res.sendStatus(403);
+  }
 });
 
 module.exports = router;
